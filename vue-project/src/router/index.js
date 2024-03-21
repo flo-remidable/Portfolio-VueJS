56e62e8db631e95a838c1b1c827deb9d8153c8e6
FlorianStrohl <strohlff66@hotmail.fr>
2024-03-21 17:37:19 +0100

    Add 404Page modal path

--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -1,5 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import PageNotFound from "../views/PageNotFound.vue";
+import Presentation from "../components/Presentation.vue";
+import Contact from "../components/Contact.vue";
+import Creation from "../components/Creation.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -8,6 +12,30 @@ const router = createRouter({
       path: "/",
       name: "home",
       component: HomeView,
+    },
+
+    {
+      path: "/presentation",
+      name: "presentation",
+      component: Presentation,
+    },
+
+    {
+      path: "/Creation",
+      name: "Creation",
+      component: Creation,
+    },
+
+    {
+      path: "/Contact",
+      name: "Contact",
+      component: Contact,
+    },
+
+    {
+      path: "/:pathMatch(.*)*",
+      name: "NotFound",
+      component: PageNotFound,
     },
   ],
 });
